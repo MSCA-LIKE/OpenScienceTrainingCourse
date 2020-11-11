5f976aef4e3c6b653925c4f910a0c9bd8f201896
Andy Clifton <andy_clifton@yahoo.com>
2020-11-11 15:45:20 +0100

    Update build_beamer.sh

--- scripts/build_beamer.sh
+++ scripts/build_beamer.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
 cd 07_seminar4/beamer
-latexmk -f -g -xelatex -verbose main.tex
-latexmk -f -g -xelatex -verbose main.tex
-latexmk -c main.tex
+latexmk -f -g -xelatex -verbose -c main.tex
 cd ~
