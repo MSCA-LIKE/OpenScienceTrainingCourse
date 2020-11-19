1556d2b43842954b912626a63041998313d4fed2
Andy Clifton <andy_clifton@yahoo.com>
2020-11-19 13:48:50 +0100

    UPDATE: Bash script

--- scripts/build_beamer.sh
+++ scripts/build_beamer.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
+cwd=$PWD
+
 cd 01_seminar1/beamer/
 latexmk -f -g -xelatex -verbose main.tex
 latexmk -c
-cd ~
 
+cd $cwd
 
 cd 07_seminar4/beamer/
 latexmk -f -g -xelatex -verbose main.tex
 latexmk -c
-cd ~
+
